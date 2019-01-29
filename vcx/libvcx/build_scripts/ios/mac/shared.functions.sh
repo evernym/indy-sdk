fd131ca0621a49964314951e1b3425ec491bf506
Ryan Marsh <ryan.marsh44@gmail.com>
2019-01-29 15:27:56 -0700

    sets var when checking for env var inside of shared.functions.sh
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- vcx/libvcx/build_scripts/ios/mac/shared.functions.sh
+++ vcx/libvcx/build_scripts/ios/mac/shared.functions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-${INDY_BRANCH:?"Indy branch needs to be set i.e (stbale or master)"}
-${INDY_VERSION:?"Indy version needs to be set"}
-${SOVTOKEN_ZIP:?"Sovtoken zip file needs to be set"}
+indyBranch=${INDY_BRANCH:?"Indy branch needs to be set i.e (stbale or master)"}
+indyVersion=${INDY_VERSION:?"Indy version needs to be set"}
+sovZip=${SOVTOKEN_ZIP:?"Sovtoken zip file needs to be set"}
 
-export LIBSOVTOKEN_IOS_BUILD_URL="https://repo.sovrin.org/ios/libsovtoken/stable/${SOVTOKEN_ZIP}"
-export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/${INDY_BRANCH}/libindy-core/${INDY_VERSION}/libindy.tar.gz"
-export LIBNULLPAY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libnullpay/${INDY_BRANCH}/libnullpay-core/${INDY_VERSION}/libnullpay.tar.gz"
+export LIBSOVTOKEN_IOS_BUILD_URL="https://repo.sovrin.org/ios/libsovtoken/stable/${sovZip}"
+export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/${indyBranch}/libindy-core/${indyVersion}/libindy.tar.gz"
+export LIBNULLPAY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libnullpay/${indyBranch}/libnullpay-core/${indyVersion}/libnullpay.tar.gz"
 
 export LIBSOVTOKEN_FILE=$(basename ${LIBSOVTOKEN_IOS_BUILD_URL})
 export LIBSOVTOKEN_VERSION=$(echo ${LIBSOVTOKEN_FILE} | cut -d'_' -f 2)
