9aa1e7d3ea9b7277175d4a5c25585995e9f1614f
Ryan Marsh <ryan.marsh44@gmail.com>
2019-01-28 17:04:03 -0700

    fixes libnullpay wget
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- vcx/ci/scripts/androidBuild.sh
+++ vcx/ci/scripts/androidBuild.sh
@@ -99,7 +99,7 @@ get_libindy() {
 		SIMPLE_LIBINDY_VERSION=$(echo ${LIBINDY_VERSION} | cut -f1 -d'-')
         if [ ! -d "libindy_${ARCH}" ]; then
 
-            wget https://repo.sovrin.org/android/libindy/${LIBINDY_BRANCH}/${LIBINDY_VERSION}/libindy_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
+            wget -q https://repo.sovrin.org/android/libindy/${LIBINDY_BRANCH}/${LIBINDY_VERSION}/libindy_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
             unzip libindy_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
 
         fi
@@ -113,7 +113,7 @@ get_libsovtoken() {
     if [ -z ${LIBSOVTOKEN_DIR} ]; then
         if [ ! -d "libsovtoken" ]; then
             echo "retrieving libsovtoken prebuilt library"
-            wget ${SOVRIN_REPO}/${LIBSOVTOKEN_ZIP}
+            wget -q ${SOVRIN_REPO}/${LIBSOVTOKEN_ZIP}
             unzip ${LIBSOVTOKEN_ZIP}
         fi
         export LIBSOVTOKEN_DIR="${PWD}/libsovtoken/${TRIPLET}"
@@ -129,7 +129,7 @@ get_libnullpay() {
 		SIMPLE_LIBINDY_VERSION=$(echo ${LIBINDY_VERSION} | cut -f1 -d'-')
         if [ ! -d "libnullpay_${ARCH}" ]; then
 
-            wget https://repo.sovrin.org/android/libindy/${LIBINDY_BRANCH}/${LIBINDY_VERSION}/libnullpay_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
+            wget -q https://repo.sovrin.org/android/libnullpay/${LIBINDY_BRANCH}/${LIBINDY_VERSION}/libnullpay_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
             unzip libnullpay_android_${ARCH}_${SIMPLE_LIBINDY_VERSION}.zip
 
         fi
