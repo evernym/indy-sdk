6d173097acf4edcb6b753577897e126d55d989a4
Ryan Marsh <ryan.marsh44@gmail.com>
2019-01-29 13:22:55 -0700

    mac.03...retrieve removes unused vars
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- vcx/libvcx/build_scripts/ios/mac/mac.03.01.libindy.retrieve.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.03.01.libindy.retrieve.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 set -e
-INDY_BRANCH=$1
-INDY_VERSION=$2
-SOVTOKEN_ZIP=$3
-source ./shared.functions.sh ${INDY_BRANCH} ${INDY_VERSION} ${SOVTOKEN_ZIP}
 
-START_DIR=$PWD
-WORK_DIR=$START_DIR/../../../../../.macosbuild
-mkdir -p $WORK_DIR
-WORK_DIR=$(abspath "$WORK_DIR")
-SHA_HASH_DIR=$START_DIR/../..
-SHA_HASH_DIR=$(abspath "$SHA_HASH_DIR")
-
+function sharedFn() {
+    INDY_BRANCH=$1
+    INDY_VERSION=$2
+    SOVTOKEN_ZIP=$3
+    source ./shared.functions.sh ${INDY_BRANCH} ${INDY_VERSION} ${SOVTOKEN_ZIP}
+}
 
 function retrieveLib() {
     library=$1
@@ -39,6 +34,8 @@ function retrieveLib() {
         rm ${version}-${file}
     fi
 }
+
+sharedFn $1 $2 $3
 retrieveLib "libindy" ${LIBINDY_VERSION} ${LIBINDY_FILE} ${LIBINDY_IOS_BUILD_URL} "tgz"
 retrieveLib "libsovtoken-ios" ${LIBSOVTOKEN_VERSION} ${LIBSOVTOKEN_FILE} ${LIBSOVTOKEN_IOS_BUILD_URL} "zip"
 retrieveLib "libnullpay" ${LIBNULLPAY_VERSION} ${LIBNULLPAY_FILE} ${LIBNULLPAY_IOS_BUILD_URL} "tgz"
