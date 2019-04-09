36ee5f9e91e3b53129af0a0d5d29054436eee8c6
Ryan Marsh <ryan.marsh44@gmail.com>
2019-04-08 18:09:16 -0600

    sanity trace statement
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- vcx/libvcx/src/api/vcx.rs
+++ vcx/libvcx/src/api/vcx.rs
@@ -290,6 +290,7 @@ pub extern fn vcx_mint_tokens(seed: *const c_char, fees: *const c_char) {
 #[no_mangle]
 pub extern fn vcx_get_current_error(error_json_p: *mut *const c_char) {
     trace!("vcx_get_current_error >>> error_json_p: {:?}", error_json_p);
+    trace!("Test for sanity!!!!");
 
     let error = get_current_error_c_json();
     trace!("in api - after get_current_er... {:?}", error);
