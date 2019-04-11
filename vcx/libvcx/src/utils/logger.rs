7817c94a60913396dbba9e545a0fe14dce5344d5
Norman Jarvis <norman.jarvis@evernym.com>
2019-04-11 12:21:54 -0600

    Changed the LibvcxLogger to just use println!.

--- vcx/libvcx/src/utils/logger.rs
+++ vcx/libvcx/src/utils/logger.rs
@@ -104,49 +104,49 @@ impl log::Log for LibvcxLogger {
     }
 
     fn log(&self, record: &Record) {
-        let mut file_2 = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .append(true)
-            .open("ryan_logs.txt").unwrap();
+        // let mut file_2 = OpenOptions::new()
+        //     .read(true)
+        //     .write(true)
+        //     .create(true)
+        //     .append(true)
+        //     .open("ryan_logs.txt").unwrap();
 
         let logDir = PathBuf::from("ryan_logs.txt");
         let mut msg = format!("Path to Ryans log: {:?}", fs::canonicalize(&logDir));
         println!("{:?}", msg);
-        file_2.write(msg.as_bytes()).unwrap();
+        // file_2.write(msg.as_bytes()).unwrap();
 
         let mut rng = rand::thread_rng();
         let my_id = rng.gen::<u32>();
         msg = format!("Ryan: Beggining of log: {:?}", my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
 
         msg = format!("\nRyan: log_cb: {:?} thread:{:?}", self.log, my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let log_cb = self.log;
 
         msg = format!("\nRyan: log_level: {:?} thread:{:?}", record.level() as u32, my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let level = record.level() as u32;
         msg = format!("\nRyan: target: {:?} thread:{:?}", record.target(), my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let target = CString::new(record.target()).unwrap();
         msg = format!("\nRyan: args: {:?} thread:{:?}", record.args(), my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let message = CString::new(record.args().to_string()).unwrap();
 
         msg = format!("\nRyan: path: {:?} thread:{:?}", record.module_path(), my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let module_path = record.module_path().map(|a| CString::new(a).unwrap());
         msg = format!("\nRyan: file: {:?} thread:{:?}", record.file(), my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let file = record.file().map(|a| CString::new(a).unwrap());
         msg = format!("\nRyan: line: {:?} thread:{:?}", record.line(), my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         let line = record.line().unwrap_or(0);
 
         msg = format!("\nRyan: Before CB: {:?}", my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
         log_cb(self.context,
                level,
                target.as_ptr(),
@@ -156,7 +156,7 @@ impl log::Log for LibvcxLogger {
                line,
         );
         msg = format!("\nRyan: after CB: {:?}\n\n\n\n\n", my_id);
-        file_2.write(msg.as_bytes()).unwrap();
+        println!("{:?}", msg);
     }
 
     fn flush(&self) {
