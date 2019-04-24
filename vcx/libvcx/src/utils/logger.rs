7fd727cc2f7879f8337d7429b2e00c14bf3b4fc9
Norman Jarvis <norman.jarvis@evernym.com>
2019-04-24 09:29:54 -0600

    Wrapped the logger callback with spawn move.

--- vcx/libvcx/src/utils/logger.rs
+++ vcx/libvcx/src/utils/logger.rs
@@ -101,14 +101,17 @@ impl log::Log for LibvcxLogger {
         //        line,
         // )
 
-        log_cb(ptr::null(),
-               1,
-               CString::new(__TARGET).unwrap().as_ptr(),
-               CString::new(__MESSAGE).unwrap().as_ptr(),
-               CString::new(__MOD_PATH).unwrap().as_ptr(),
-               CString::new(__FILE_PATH).unwrap().as_ptr(),
-               45
-        )
+        spawn(move|| {
+            log_cb(ptr::null(),
+                1,
+                CString::new(__TARGET).unwrap().as_ptr(),
+                CString::new(__MESSAGE).unwrap().as_ptr(),
+                CString::new(__MOD_PATH).unwrap().as_ptr(),
+                CString::new(__FILE_PATH).unwrap().as_ptr(),
+                45
+            );
+            Ok(())
+        });
     }
 
     fn flush(&self) {
