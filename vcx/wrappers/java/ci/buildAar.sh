f6eae33426cae0c0c03dde3aaf5f2f2d7624ca51
Norman Jarvis <norman.jarvis@evernym.com>
2019-04-29 17:23:56 -0600

    Adjusting for the vagrant environment.

--- vcx/wrappers/java/ci/buildAar.sh
+++ vcx/wrappers/java/ci/buildAar.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH=${BASH_SOURCE[0]}      # this script's name
 SCRIPT_NAME=${SCRIPT_PATH##*/}       # basename of script (strip path)
 SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
 
-export ANDROID_BUILD_TOOLS="/home/android/android-sdk-linux/build-tools/27.0.3"
+export ANDROID_BUILD_TOOLS="/home/vagrant/android-sdk-linux/build-tools/27.0.3"
 export ANDROID_BUILD_FOLDER="/tmp/android_build"
 echo "ANDROID_BUILD_FOLDER: ${ANDROID_BUILD_FOLDER}"
 ANDROID_SDK=${ANDROID_BUILD_FOLDER}/sdk
